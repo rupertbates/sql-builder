a5ab666dbb01072d71c409de1da3df13f2d2fdfd
rbates <rupert.bates@gmail.com>
2015-05-30 15:39:17 -0700

    Android test insert & select

---
+++ app/src/androidTest/java/com/theguardian/androidsqlitebuilder/TestData.java
@@ -0,0 +1,8 @@
+package com.theguardian.androidsqlitebuilder;
+
+public class TestData {
+    public static final String RUPERT = "Rupert";
+    public static final String BATES = "Bates";
+    public static final float AGE = 99F;
+    public static final int ID = 1;
+}
--- app/src/androidTest/java/com/theguardian/androidsqlitebuilder/UserTableTests.java
+++ app/src/androidTest/java/com/theguardian/androidsqlitebuilder/UserTableTests.java
@@ -4,19 +4,50 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
+import static com.theguardian.androidsqlitebuilder.UserDBHelper.*;
+import static com.theguardian.androidsqlitebuilder.UserDBHelper.USER_TABLE;
+import static com.theguardian.sql.SqlBuilder.insert;
+import static com.theguardian.sql.SqlBuilder.select;
+
 public class UserTableTests extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getContext().deleteDatabase(USER_DB);
+    }
+
     public void testUserTableCreation() {
         UserDBHelper helper = new UserDBHelper(getContext());
         SQLiteDatabase db = helper.getReadableDatabase();
-        Cursor cursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '" + UserDBHelper.USER_TABLE + "'", null);
+        Cursor cursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '" + USER_TABLE + "'", null);
         assertNotNull(cursor);
         assertEquals(1, cursor.getCount());
         cursor.close();
     }
 
-    public void testInsert(){
+    public void testInsert() {
         UserDBHelper helper = new UserDBHelper(getContext());
         SQLiteDatabase db = helper.getWritableDatabase();
-        //db.execSQL(insert());
+        db.execSQL(insert()
+                        .into(USER_TABLE)
+                        .values(TestData.ID, TestData.RUPERT, TestData.BATES, TestData.AGE)
+                        .build()
+        );
+
+        String query = select(Fields.ID, Fields.FIRSTNAME, Fields.SURNAME, Fields.AGE)
+                .from(USER_TABLE)
+                .build();
+
+        Cursor cursor = helper.getReadableDatabase().rawQuery(query, null);
+
+        assertEquals(1, cursor.getCount());
+        assertTrue(cursor.moveToFirst());
+        assertEquals(TestData.ID, cursor.getInt(cursor.getColumnIndex(Fields.ID)));
+        assertEquals(TestData.RUPERT, cursor.getString(cursor.getColumnIndex(Fields.FIRSTNAME)));
+        assertEquals(TestData.BATES, cursor.getString(cursor.getColumnIndex(Fields.SURNAME)));
+        assertEquals(TestData.AGE, cursor.getFloat(cursor.getColumnIndex(Fields.AGE)));
+        cursor.close();
+
     }
 }
